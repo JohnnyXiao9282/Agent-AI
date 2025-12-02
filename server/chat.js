f5beaeebd1bcc5356104c52401173f6befac759d
Johnny Xiao <yuxiao9282@gmail.com>
2025-12-01 19:30:42 -0800

    add uploads

--- server/chat.js
+++ server/chat.js
@@ -5,3 +5,22 @@ import { ChatOpenAI } from "@langchain/openai";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
+const chat = async (filePath = "./uploads/hbs-lean-startup.pdf", query) => {
+    // Get API key from environment
+  const apiKey = process.env.OPENAI_API_KEY;
+
+  // Step 1
+  const loader = new PDFLoader(filePath);
+
+  const data = await loader.load();
+
+  // Step 2
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500, //  (in terms of number of characters)
+    chunkOverlap: 0,
+  });
+
+  const splitDocs = await textSplitter.splitDocuments(data);
+
+
+}
