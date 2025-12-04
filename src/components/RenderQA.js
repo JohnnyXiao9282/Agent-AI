7e0d26eb7224863abb30cb3051cf81e6d4091a3b
Johnny Xiao <yuxiao9282@gmail.com>
2025-12-03 20:01:13 -0800

    add render qa file

---
+++ src/components/RenderQA.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Spin } from "antd";
+
+const containerStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  flexDirection: "column",
+  marginBottom: "20px",
+};
+
+const userContainer = {
+  textAlign: "right",
+};
+
+const agentContainer = {
+  textAlign: "left",
+};
+
+const userStyle = {
+  maxWidth: "50%",
+  textAlign: "left",
+  backgroundColor: "#1677FF",
+  color: "white",
+  display: "inline-block",
+  borderRadius: "10px",
+  padding: "10px",
+  marginBottom: "10px",
+};
+
+const answerContainer = {
+  marginBottom: "10px",
+};
+
+const answerLabel = {
+  fontSize: "12px",
+  fontWeight: "bold",
+  color: "#666",
+  marginBottom: "5px",
+};
+
+const ragAnswerStyle = {
+  maxWidth: "50%",
+  textAlign: "left",
+  backgroundColor: "#E6F7FF",
+  color: "black",
+  display: "inline-block",
+  borderRadius: "10px",
+  padding: "10px",
+  marginBottom: "5px",
+  borderLeft: "4px solid #1890FF",
+};
+
+const mcpAnswerStyle = {
+  maxWidth: "50%",
+  textAlign: "left",
+  backgroundColor: "#F6FFED",
+  color: "black",
+  display: "inline-block",
+  borderRadius: "10px",
+  padding: "10px",
+  marginBottom: "5px",
+  borderLeft: "4px solid #52C41A",
+};
+
